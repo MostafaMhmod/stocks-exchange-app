d564196ac83b249603c0150c2ccb725305b8d3d7
Moustafa.mahm@gmail.com <moustafa.mahm@gmail.com>
2021-02-07 03:35:38 +0200

    fixing db models bugs

--- thndr/models/holding.py
+++ thndr/models/holding.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Holding(models.Model):
-    user = models.OneToOneField(
+    user = models.ForeignKey(
         User, verbose_name=("user"), on_delete=models.CASCADE
     )
     stock = models.ForeignKey('thndr.Stock', verbose_name=("stock"), on_delete=models.CASCADE)
